f90980e13aff92a5dbf8514fac20b109f79da373
yuvan20 <115212455+yuvan20@users.noreply.github.com>
2026-07-22 13:40:07 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {}
-variable "environment" {}
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "availability_zone" {}
-variable "ami_id" {}
-variable "instance_type" {}
